dda74f87bfc5afec1444a4300b01e251555bc58f
TronBlack <TronBlack@gmail.com>
2018-10-05 16:31:53 -0600

    fix blockfacts

--- assets/tools/blockfacts.py
+++ assets/tools/blockfacts.py
@@ -11,11 +11,18 @@ cli = "raven-cli"
 
 #mode = "-testnet"
 mode = ""
-rpc_port = 8766
+rpc_port = 18766
 #Set this information in your raven.conf file (in datadir, not testnet3)
 rpc_user = 'rpcuser'
 rpc_pass = 'rpcpass555'
 
+def get_rpc_connection():
+    from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+    connection = "http://%s:%s@127.0.0.1:%s"%(rpc_user, rpc_pass, rpc_port)
+    rpc_conn = AuthServiceProxy(connection)
+    return(rpc_conn)
+
+rpc_connection = get_rpc_connection()
 
 def rpc_call(params):
     process = subprocess.Popen([cli, mode, params], stdout=subprocess.PIPE)
@@ -28,6 +35,10 @@ def get_blockinfo(num):
     blockinfo = rpc_connection.getblock(hash)
     return(blockinfo)
 
+def get_bci():
+    bci = rpc_connection.getblockchaininfo()
+    return(bci)    
+
 def get_rpc_connection():
     from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
     connection = "http://%s:%s@127.0.0.1:%s"%(rpc_user, rpc_pass, rpc_port)
@@ -35,11 +46,20 @@ def get_rpc_connection():
     rpc_connection = AuthServiceProxy(connection)
     return(rpc_connection)
 
-for i in range(1,1000):
+#Get the blockheight of the chain
+blockheight = get_bci().get('blocks')
+
+max_sz = 0
+for i in range(1,blockheight):
     dta = get_blockinfo(i)
     print("Block #" + str(i))
     print(dta.get('hash'))
     print(dta.get('difficulty'))
     print(dta.get('time'))
+    sz = dta.get('size')
+    if (sz > max_sz):
+        max_sz = sz
+    print(str(sz))
     print("")
 
+print('Max size: ' + str(max_sz))
